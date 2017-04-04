e5a2ff81fff12e7c886c7f6c0923268e8b17391c
Marco Silva <marco.silva@bytewood.com>
2017-04-03 21:52:03 -0300

    playing around at the end of the day

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+const browserSync = require('browser-sync');
+const childProcess = require('child_process');
+const gulp = require('gulp');
+const gulpUtil = require('gulp-util');
+
+const postcss = require('gulp-postcss');
+const cssImport = require('postcss-import');
+const nest = require('postcss-nested');
+const customProps = require('postcss-custom-properties');
+const calc = require('postcss-calc');
+const nano = require('gulp-cssnano');
+
+
+// Build jekyll project
+gulp.task('jekyll', done => {
+  gulpUtil.log("process-platform : " + process.platform);
+  
+  var isWin = /^win/.test(process.platform);
+  var execScript;
+  
+  if (isWin){
+	  execScript = "jekyll.bat";
+  }else{
+	  execScript = "jekyll";
+	  
+  }
+  
+  gulpUtil.log("jekyll script : " + execScript);
+  
+  childProcess.spawn(execScript, ['build', '--drafts', '--source', './docs', '--destination', './docs/_site', '--future'], { stdio: 'inherit' }).on('close', done);
+});
+
+// Rebuild and refresh project
+gulp.task('reload', ['jekyll'], () => {
+  browserSync.reload();
+});
+
+// Start BrowserSync server and serve _site directory
+gulp.task('browser-sync', ['styles', 'jekyll'], () => {
+  browserSync.init({
+    ui: false,
+    ghostMode: {
+      clicks: true,
+      forms: false,
+      scroll: true
+    },
+    logPrefix: 'dreamcaster',
+    notify: false,
+    // port: 4000,
+    server: {
+      baseDir: './docs/_site'
+    }
+  });
+  
+  gulp.watch('./docs/_src/css/**/*.css', ['styles', 'reload']);
+  gulp.watch(['./docs/index.html', './docs/_layouts/*.html', './docs/_includes/*.html', './docs/_posts/*', './docs/_drafts/*', './docs/*.md'], ['reload']);
+
+});
+
+// Process css, autoprefix, minify
+gulp.task('styles', () => {
+  const processors = [
+    cssImport,
+    customProps,
+    calc,
+    nest
+  ];
+  return gulp.src('./docs/_src/css/main.css')
+    .pipe(postcss(processors))
+    .pipe(nano({
+          //the following option is for :CSS3 Animation is missing after minify
+          reduceIdents: {
+              keyframes: false
+          },
+          discardUnused: {
+              keyframes: false
+          }
+          })
+    )
+    .pipe(gulp.dest('./docs/_includes/css'));
+});
+
+// Watch sass and all html posts
+gulp.task('watch', () => {
+  gulp.watch('./docs/_src/css/**/*.css', ['styles', 'reload']);
+  gulp.watch(['./docs/index.html', './docs/_layouts/*.html', './docs/img/*', './docs/_includes/*.html', './docs/_posts/*', './docs/_drafts/*', './docs/*.md'], ['reload']);
+});
+
+// default task
+gulp.task('default', ['styles', 'jekyll', 'browser-sync']);
